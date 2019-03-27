ddb47b038c45b72cf3bb0635a452cee1e0e5fd10
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2019-03-26 21:52:37 -0300

    Commit dos estudos

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux'
+import TodoReducer from '../todo/todoReducer'
+
+const rootReducer = combineReducers({
+    todo: TodoReducer
+})
+
+export default rootReducer
